61aff038eaa3f67ed84b5b1b5fe52d7c138c1caa
flaubersauan <flaubersauan3@gmail.com>
2025-09-17 13:41:37 -0300

    criação da estrutura básica do projeto

--- app.py
+++ app.py
@@ -6,6 +6,25 @@ app = Flask(__name__)
 
 # cria a parte de config variavel 
 
+# criação das rotas 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/cadastro')
+def register():
+    return render_template('cadastro.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dash():
+    return render_template('dashboard.html')
+
 # um arq .env (sugestao )
 
 
